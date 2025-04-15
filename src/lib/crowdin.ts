10454e44ad880d09c8795f66b644a3cedc6b7c6d
ModzabazeR <moddaeng.pps@gmail.com>
2025-04-15 15:39:36 +0700

    update

--- src/lib/crowdin.ts
+++ src/lib/crowdin.ts
@@ -1,15 +1,13 @@
-import type Client from '@crowdin/crowdin-api-client';
-import crowdin, { type Credentials } from '@crowdin/crowdin-api-client';
+import type Client from "@crowdin/crowdin-api-client";
+import crowdin, { type Credentials } from "@crowdin/crowdin-api-client";
 
 // credentials
 const credentials: Credentials = {
-	token: import.meta.env.CROWDIN_API_TOKEN,
+  token: import.meta.env.CROWDIN_API_TOKEN,
 };
 
-const {
-	translationStatusApi,
-} = new (crowdin as any).default(credentials) as Client; // why doesn't Crowdin update their SDK typings...
+const { translationStatusApi } = new (crowdin as any).default(
+  credentials
+) as Client; // why doesn't Crowdin update their SDK typings...
 
-export {
-	translationStatusApi
-}
+export { translationStatusApi };
--- src/pages/api/progress.ts
+++ src/pages/api/progress.ts
@@ -1,39 +1,45 @@
-import type { APIRoute } from 'astro';
-import { translationStatusApi } from '../../lib/crowdin';
-import type { ResponseList, TranslationStatusModel } from '@crowdin/crowdin-api-client';
+import type { APIRoute } from "astro";
+import { translationStatusApi } from "../../lib/crowdin";
+import type {
+  ResponseList,
+  TranslationStatusModel,
+} from "@crowdin/crowdin-api-client";
 
 export const prerender = false;
 
-declare module '@crowdin/crowdin-api-client' {
-	interface TranslationStatus {
-		getProjectProgress(projectId: number, options?: { languageIds?: string }): Promise<ResponseList<TranslationStatusModel.LanguageProgress>>;
-	}
+declare module "@crowdin/crowdin-api-client" {
+  interface TranslationStatus {
+    getProjectProgress(
+      projectId: number,
+      options?: { languageIds?: string }
+    ): Promise<ResponseList<TranslationStatusModel.LanguageProgress>>;
+  }
 }
 
 const PROJECT_ID = 483903;
 
-export const GET: APIRoute = async ({ params, request }) => {
-	const response = await translationStatusApi.getProjectProgress(PROJECT_ID, {
-		languageIds: "th"
-	})
-	const data = response.data[0].data
+export const GET: APIRoute = async () => {
+  const response = await translationStatusApi.getProjectProgress(PROJECT_ID, {
+    languageIds: "th",
+  });
+  const data = response.data[0].data;
 
-	const { total, translated, approved } = data.words
+  const { total, translated, approved } = data.words;
 
-	// calculate percentage
-	let translateProgress = (translated / total) * 100
-	let approveProgress = (approved / total) * 100
+  // calculate percentage
+  let translateProgress = (translated / total) * 100;
+  let approveProgress = (approved / total) * 100;
 
-	translateProgress = parseFloat(translateProgress.toFixed(4))
-	approveProgress = parseFloat(approveProgress.toFixed(4))
+  translateProgress = parseFloat(translateProgress.toFixed(4));
+  approveProgress = parseFloat(approveProgress.toFixed(4));
 
-	return new Response(
-		JSON.stringify({
-			total,
-			translated,
-			approved,
-			translateProgress,
-			approveProgress
-		})
-	);
-}
+  return new Response(
+    JSON.stringify({
+      total,
+      translated,
+      approved,
+      translateProgress,
+      approveProgress,
+    })
+  );
+};
