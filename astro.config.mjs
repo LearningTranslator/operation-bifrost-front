25f5dd2350d61a5911da01103398afe16a9a485f
ModzabazeR <moddaeng.pps@gmail.com>
2025-04-15 20:22:46 +0700

    move to wrangler

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import svelte from "@astrojs/svelte";
-
-import vercel from "@astrojs/vercel";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte()],
   output: "server",
-  adapter: vercel(),
+  adapter: cloudflare(),
   vite: {
     plugins: [tailwindcss()]
   }
--- src/lib/crowdin.ts
+++ src/lib/crowdin.ts
@@ -1,4 +1,3 @@
-import type Client from "@crowdin/crowdin-api-client";
 import crowdin, { type Credentials } from "@crowdin/crowdin-api-client";
 
 // credentials
@@ -6,8 +5,6 @@ const credentials: Credentials = {
   token: import.meta.env.CROWDIN_API_TOKEN,
 };
 
-const { translationStatusApi } = new (crowdin as any).default(
-  credentials
-) as Client; // why doesn't Crowdin update their SDK typings...
+const { translationStatusApi } = new crowdin(credentials);
 
 export { translationStatusApi };
